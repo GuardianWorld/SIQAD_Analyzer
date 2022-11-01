a163703ee3a7d467d941529eefbb4568f3a68b22
(Emanuel) <12464623+GuardianWorld@users.noreply.github.com>
2022-11-01 09:12:55 -0300

    Update menu.h

--- header/menu.h
+++ header/menu.h
@@ -4,6 +4,11 @@
 #include <limits>
 #include "OSFileCorrector.h"
 using namespace std;
+		
+/**
+* This file simply has the menu functions for all the interactions, think of it as a "View" setup equivalent of MVC.
+* //TODO: Documentation of each functions.
+*/
 
 #define noConversion "999"
 
